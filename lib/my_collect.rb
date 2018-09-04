7a323ec4753aef475ea810f82136cd32658913a1
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-04 16:42:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
 
+counter = 0
+collection2 = []
+while counter<collection.length
+  element = collection[counter]
+  collection2 << yield(element)
+  #
+  counter+=1
+end
+collection2
 
+end
